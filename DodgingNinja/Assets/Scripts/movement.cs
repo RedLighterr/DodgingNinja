b226ae7aeed823f293494cafcdf4f4235b40f6ac
Muhammed Furkan Yüce <89073406+RedLighterr@users.noreply.github.com>
2023-01-03 10:14:25 +0300

    changes again yes changes

--- DodgingNinja/Assets/Scripts/movement.cs
+++ DodgingNinja/Assets/Scripts/movement.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
 public class movement : MonoBehaviour
 {
     Rigidbody rb;
